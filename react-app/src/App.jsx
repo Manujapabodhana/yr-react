1e79e9485d71d15d0714fcccc7c7050a4115fde7
manuja <rpmanuja123@gmail.com>
2025-05-21 07:28:10 +0530

    jj

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,7 +3,17 @@ import Mychannel from './Components/MyChannel/Mychannel'; // Make sure the path 
 function App() {
   return (
     <>
-      <di
+      <div id="wrapper">
+        <div id="navconent">
+        <h3>header content</h3>
+      </div>
+      <div id="bodycontent">
+        <p>main side content</p>
+      </div>
+      <div id="footercontent">
+        <p>footer content</p>
+      </div>
+      </div>
     </>
   );
 }
