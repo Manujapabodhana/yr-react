ff028f91ea263263dc37a45b6564dbc857b5a751
manuja <rpmanuja123@gmail.com>
2025-05-21 12:56:33 +0530

    gg

--- react-app/src/Components/FooterContent/FooterContent.jsx
+++ react-app/src/Components/FooterContent/FooterContent.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './footerContent.css'
 
 function FooterContent() {
   return (
--- react-app/src/Components/HeaderContent/HeadrContent.jsx
+++ react-app/src/Components/HeaderContent/HeadrContent.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './HeaderContent.css';
+
 
 function HeaderContent() {
   return (
